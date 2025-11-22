2ca1cb990929d2f12d1e47ea9f62d44b66706ebe
Anton Bashmakov <anton.bashmakov@platform24.se>
2025-11-22 10:37:54 +0100

    add storybook

---
+++ crm/.storybook/vitest.setup.ts
@@ -0,0 +1,6 @@
+import { setProjectAnnotations } from '@storybook/preview-api';
+import * as a11yAddonAnnotations from '@storybook/addon-a11y/preview';
+
+setProjectAnnotations([
+  a11yAddonAnnotations,
+]);
