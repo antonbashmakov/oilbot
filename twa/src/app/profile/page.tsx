cc3e380182c8eec2bee621fd52874cebf77fccfa
Anton Bashmakov <anton.bashmakov@platform24.se>
2026-01-11 18:12:44 +0100

    small visual fix

--- twa/src/app/profile/page.tsx
+++ twa/src/app/profile/page.tsx
@@ -124,7 +124,7 @@ export default function ProfilePage() {
           </div>
           <div className="flex items-baseline justify-between">
             <p className="text-primary text-4xl font-extrabold tracking-tight">
-              {customer?.balance?.value ? `${-customer.balance.value.toFixed(2)} ₽` : '0.00 ₽'}
+              {customer?.balance?.value ? `${customer.balance.value.toFixed(2)} ₽` : '0.00 ₽'}
             </p>
             {/*<button className="bg-primary/10 hover:bg-primary/20 text-primary px-4 py-2 rounded-full text-sm font-bold transition-colors flex items-center gap-1">
               <span className="material-symbols-outlined text-[18px]">add</span>
